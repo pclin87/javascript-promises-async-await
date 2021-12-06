78dd1a8ae441830d4f95f00a4596bac87468470e
pclin87 <73913675+pclin87@users.noreply.github.com>
2021-12-06 13:33:21 -1000

    test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import { fetchWithTimeout } from './services'
+import { fetchMovies } from './services'
+import { fetchBooks } from './services'
 const movies = require('./data/movies.json')
+
+
+function getBooksAndMovies() {
+    return Promise.all([fetchBooks(), fetchMovies()])
+    .then(([books, movies ]) => ({
+        books,
+        movies
+      }))
+      .catch(error => console.log("Error fetching books and movies", error));
+}
