4c4221193e393fa5a4a725a0b42b194198bb3105
pclin87 <73913675+pclin87@users.noreply.github.com>
2021-12-06 13:21:37 -1000

    1

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ export function fetchMovies() {
 }
 
 const moviePromise = fetchMovies()
-moviePromise.then(results => console.log(results))
+moviePromise.then(results => {
+    console.log(results)
+})
