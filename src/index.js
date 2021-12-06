4b3825190fca3796113d2aca12e58ebd3e97948f
Peter Lin <plin@petroiq.com>
2021-12-06 11:06:10 -1000

    5

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-export function fetchMovies() {}
+import { fetchWithTimeout } from './services'
+export function fetchMovies() {
+    const resolveFunction = () => movies
+}
 var movies = require('./data/movies.json')
